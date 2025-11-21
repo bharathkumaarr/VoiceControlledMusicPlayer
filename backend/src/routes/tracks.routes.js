be3604616a395fe6a17796de7324adad0f821e79
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-11-21 14:04:53 +0530

    search route error resolved

--- backend/src/routes/tracks.routes.js
+++ backend/src/routes/tracks.routes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const trackController = require('../controllers/trackController')
 
 router.get('/', trackController.listTracks)
-router.get('/stream', trackController.searchTrack)
+router.get('/search', trackController.searchTrack)
 router.get('/:id', trackController.getTrack)
 router.get('/:id/stream', trackController.streamTrack)
 
