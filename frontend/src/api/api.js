3cab9cd4bfbcb365b9d5c54f963258e947f82b17
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-11-21 16:03:39 +0530

    api helper added

---
+++ frontend/src/api/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api',
+    timeout: 10000,
+
+})
+
+export default api;
+
+
+
